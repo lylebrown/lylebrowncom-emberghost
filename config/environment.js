1b2eed4301db27aac14cf0833ef265f152dfe2d9
Lyle Brown <lyle@lylebrown.com>
2019-03-15 13:34:10 -0700

    Rename site and link to about page

--- config/environment.js
+++ config/environment.js
@@ -24,17 +24,17 @@ module.exports = function(environment) {
     },
 
     blog: {
-      title: 'empress-blog',
-      description: 'Static Blog System - Built with Ember',
+      title: 'Lyle Brown',
+      description: 'Lyle Brown\'s Personal Site',
       coverImage: '/images/blog-cover.jpg',
 
       navigation: [{
         label: 'Home',
         route: 'index'
       }, {
-        label: 'Built by Chris Manson',
+        label: 'About',
         route: 'page',
-        id: 'chris-manson'
+        id: 'about'
       }]
     }
   };
